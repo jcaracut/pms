c1124204dfc02fc5dcef859f23b7533db8ede477
jecsoncaracut <jcaracut@yondu.com>
2022-08-04 22:33:12 +0800

    added qr code scanner component

--- src/app/features/dealers/pages/table/components/scanner/scanner.component.ts
+++ src/app/features/dealers/pages/table/components/scanner/scanner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-scanner',
@@ -7,8 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScannerComponent implements OnInit {
 
+  scan_started: boolean;
+
   constructor() { }
 
   ngOnInit() {}
+
+  async checkPermission() {
+    return new Promise(async (resolve, reject) => {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        resolve(true);
+      } else if (status.denied) {
+        BarcodeScanner.openAppSettings();
+        resolve(false);
+      }
+    });
+  }
+
+  async startScanner() {
+    const allowed = await this.checkPermission();
+
+    if (allowed) {
+      this.scan_started = true;
+      BarcodeScanner.hideBackground();
+
+      const result = await BarcodeScanner.startScan();
+
+      if (result.hasContent) {
+        this.scan_started = false;
+        alert(result.content); //The QR content will come out here
+        //Handle the data as your heart desires here
+      } else {
+        alert('NO DATA FOUND!');
+      }
+    } else {
+      alert('NOT ALLOWED!');
+    }
+  }
+
+  stopScanner() {
+    BarcodeScanner.stopScan();
+    this.scan_started = false;
+  }
+
+  ionViewWillLeave() {
+    BarcodeScanner.stopScan();
+    this.scan_started = false;
+  }
 
 }
